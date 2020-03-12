97b786d6a9472c937c2d8a74e20cb6abbf06a380
allanDu <allanbum@gmail.com>
2020-03-12 15:53:24 -0700

    Jasmine ControllerAdm

--- spec/models/adm_spec.js
+++ spec/models/adm_spec.js
@@ -1,6 +1,14 @@
 var Adm = require('../../app/models/adm');
 
+describe('Modelo Adm', function() {
 it('Deve retornar o modelo de Adm', ()=> {
     let adm = Adm;
-    expect(adm =! undefined).toBe(true);
+    expect(adm !== undefined).toBe(true);
+    expect(adm.id !== undefined).toBe(true);
+    expect(adm.nome !== undefined).toBe(true);
+    console.log("========================")
+    console.log(typeof adm.todos() == Array)
+    console.log("========================")
+    expect(adm.todos() !== undefined).toBe(true);
     });
+});
